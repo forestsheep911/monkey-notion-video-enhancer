c2e10c18573dabde0b10fa41086ef1b79dd62fa9
徐冰 <38274876+forestsheep911@users.noreply.github.com>
2024-02-21 12:30:12 +0800

    init

--- config/dev.meta.js
+++ config/dev.meta.js
@@ -3,7 +3,7 @@ const { resolve } = require('path')
 module.exports = {
   name: `${pj.name}`,
   namespace: `https://github.com/${pj.author}/monkey-${pj.name}`,
-  match: ['https://*'],
-  grant: ['GM_getValue', 'GM_setValue', 'GM_addValueChangeListener'],
+  match: ['https://boccaro.notion.site/*'],
+  grant: ['GM_getValue', 'GM_setValue', 'GM_addValueChangeListener', 'GM_getResourceText', 'GM_addStyle'],
   require: [`file://${resolve(__dirname, '../dist/dev').replaceAll('\\', '/')}/${pj.name}.script.js`],
 }
--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,21 @@
+import Plyr from 'plyr'
+
 const app = () => {
   console.log('monkey jumping on the bed.')
+  // const css = GM_getResourceText('plyrcss')
+  // GM_addStyle(css)
+  setTimeout(() => {
+    const videoBlock = document.querySelectorAll('video')
+    console.log(videoBlock)
+
+    videoBlock.forEach((video) => {
+      const videoClone = video.cloneNode(true) as HTMLElement
+      document.body.appendChild(videoClone)
+      console.log(videoClone)
+
+      new Plyr(videoClone)
+    })
+  }, 5000)
 }
 
 export default app
